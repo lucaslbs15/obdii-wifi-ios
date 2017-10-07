edf558deb3305429c7f2d0107a619a9c15e191a8
Lucas Bicca <lucaslbs15@gmail.com>
2017-10-07 14:12:33 -0300

    First commit

---
+++ OBD II wifi/OBD II wifi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OBD II wifi
+//
+//  Created by Lucas Bicca on 07/10/17.
+//  Copyright © 2017 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
